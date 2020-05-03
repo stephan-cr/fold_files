7065213c27670fa7b1355674db751fc3cae81216
stephan_cr <stephan.cr@gmx.de>
2020-05-03 21:27:07 +0200

    add command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![warn(rust_2018_idioms)]
 
+use std::env;
 use std::fs::{metadata, read_dir, DirEntry};
+use std::io::{self, StdoutLock, Write};
 use std::os::linux::fs::MetadataExt;
 use std::path::Path;
+
+use clap::{App, Arg};
 
 fn transform_filename(filename: &str) -> String {
     let mut transformed = filename.replace("/", ";");
@@ -13,7 +17,7 @@ fn transform_filename(filename: &str) -> String {
     transformed
 }
 
-fn visit(path: &Path, cb: &dyn Fn(&DirEntry), dev: Option<u64>) {
+fn visit(path: &Path, cb: &dyn Fn(&DirEntry), dev: Option<u64>, mut stdout: &mut StdoutLock<'_>) {
     let dev = if dev == None {
         Some(metadata(&path).unwrap().st_dev())
     } else {
@@ -32,16 +36,46 @@ fn visit(path: &Path, cb: &dyn Fn(&DirEntry), dev: Option<u64>) {
             }
             if let Some(ref filename_str) = path.to_str() {
                 let transformed = transform_filename(filename_str);
-                println!("{} {}", transformed, size);
+                stdout
+                    .write_fmt(format_args!("{} {}\n", transformed, size))
+                    .unwrap();
             }
         } else if path.is_dir() {
-            visit(&path, &cb, dev);
+            visit(&path, &cb, dev, &mut stdout);
         }
     }
 }
 
-fn main() {
-    visit(&Path::new("."), &|_x| {}, None);
+fn main() -> Result<(), io::Error> {
+    let matches = App::new(
+        Path::new(&env::args().next().unwrap())
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap(),
+    )
+    .version("0.1.0")
+    .arg(
+        Arg::with_name("xdev")
+            .help("Do not descend into directories on other filesystems")
+            .long("xdev")
+            .takes_value(false),
+    )
+    .arg(
+        Arg::with_name("DIRECTORY")
+            .help("Sets the root directory to use")
+            .required(true)
+            .index(1),
+    )
+    .get_matches();
+    let _stdout = io::stdout();
+    let mut stdout = _stdout.lock();
+    if let Some(ref value) = matches.value_of("DIRECTORY") {
+        visit(&Path::new(value), &|_x| {}, None, &mut stdout);
+    }
+    stdout.flush()?;
+
+    Ok(())
 }
 
 #[cfg(test)]
